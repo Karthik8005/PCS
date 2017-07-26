23890696af53c3ffa3095dd8b88d436ec83ffcf0
LAPTOP-C2MMHV5F\Karthik Reddy Aleti <karthik77.java@gmail.com>
2017-07-25 20:18:59 -0400

    pcs added

--- src/main/java/com/pcs/services/entities/EmployeeDTO.java
+++ src/main/java/com/pcs/services/entities/EmployeeDTO.java
@@ -1,20 +1,27 @@
 package com.pcs.services.entities;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 @Document(collection="pcs_employess")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"id","name"})
+
 public class EmployeeDTO {
 	
-	@Id
+@JsonProperty
 	private String id;
-	
-	@TextIndexed
+@JsonProperty
 	private String name;
 	
-	@TextIndexed 
+@JsonProperty
 	private String village;
 	
 	public EmployeeDTO(String name,String village){
@@ -35,32 +42,40 @@ public class EmployeeDTO {
 		super();
 	}
 
+	@JsonAnyGetter
 	public String getId() {
 		return id;
 	}
 
+	@JsonAnySetter
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	@JsonAnyGetter
 	public String getName() {
 		return name;
 	}
 
+	@JsonAnySetter
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	@JsonAnyGetter
 	public String getVillage() {
 		return village;
 	}
 
+	@JsonAnySetter
 	public void setVillage(String village) {
 		this.village = village;
 	}
 	
 	
-	
+	/*
+	 * @TextIndexed
+	 */
 	
 
 }
